e5562d91fb4020da52f6e9c9f61f9a85eb4780c2
hamza <mohammadhamza117@gmail.com>
2023-06-16 10:41:12 +0500

    first commit

---
+++ src/data/industryInsightsData.js
@@ -0,0 +1,43 @@
+export const IndustryInsightsData = [
+    {
+        name: "Component Manufacturers",
+        description: "From power relays to charging cords, metering devices to transmission couplers, electronics manufacturers the world over are building industry specific solutions."
+    },
+    {
+        name: "Electric Vehicle Charging Infrastructure",
+        description: "Electric Vehicle Charging Infrastructure (EVCI) comprises a system of power electronics, sensors, conductors and couplers that enable the safe transmission of energy to a vehicle battery."
+    },
+    {
+        name: "Charge Point Operators",
+        description: "Charge Point Operators (CPOs) own and operate charging infrastructure often utilizing network services to control driver access, monitor system status and generate revenue."
+    },
+    {
+        name: "Electric Mobility Service Providers",
+        description: "Electric Mobility Service Providers (EMSPs) connect drivers with CPO infrastructure via customer tokens, vehicle serialization or  mobile applications to enable charging often for a fee."
+    },
+    {
+        name: "Smart Charging",
+        description: "Smart charging is a software based system provided remotely via a CMS or locally within the EVCI that enables dynamic balancing of energy load across a charging environment and/or with building management systems.  "
+    },
+    {
+        name: "Installation Providers",
+        description: "The development, design and construction of  high-power charging systems involves engineering, permitting, excavation, structural core drilling, utility interconnections, equipment calibration and optimization of communication system integrity."
+    },
+    {
+        name: "Payment Processors",
+        description: "Collection of revenue for charging services is enabled via local credit card terminals or cloud-based payment system providers.  Various transaction fees and regulated security protocols may apply."
+    },
+    {
+        name: "Communications Providers",
+        description: "Charging networks and interconnected charging infrastructure systems require robust communications systems.  Local messaging can be enabled via corded r wireless systems.  CMS backhaul requires use of Internet or Cellular Service Providers."
+    },
+    {
+        name: "Energy",
+        description: "Charging ecosystems are energy intensive operations and utility demand charges are the predominant cost driver in the eMobility ecosystem.  Demand charges can be mitigated via smart charging, local energy storage and behind the meter generation."
+    },
+    {
+        name: "Charger Management Systems",
+        description: "Charger Management Systems (CMS) are cloud-based networks that use open or proprietary communication protocols and APIs to connect charging infrastructure with administrative control dashboards."
+    },
+
+]
---
+++ src/pages/home/Index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Products from "../../components/products/Index";
+import dummyVideo from "../../assets/images/atoms.mp4";
+
+const Index = () => {
+  return (
+      <Products />
+  );
+};
+
+export default Index;
